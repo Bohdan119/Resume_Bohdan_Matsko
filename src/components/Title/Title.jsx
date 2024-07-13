670827d6457461692f3994a8c6a9577d9b28bb85
Bohdan Matsko <bm01271970@gmail.com>
2024-07-13 21:12:30 +0200

    +

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -7,14 +7,14 @@ const Title = () => {
       <h1 className={css.mainTitle}>BOHDAN MATSKO</h1>
       <h2 className={css.mainDirection}>
         Frontend Developer (junior) with great ambitions!
+      </h2>
         <hr className={ css.line} />
-        <span className={css.mainParagraph}>
+        <p className={css.mainParagraph}>
           Able to understand technologies from complex distillation column to
           complex code! I aspire to develop in the field of IT, at the same time
           I am studying English, and I am also continuing my studies at the GoIT
           school
-        </span>
-      </h2>
+        </p>
     </div>
   );
 }
