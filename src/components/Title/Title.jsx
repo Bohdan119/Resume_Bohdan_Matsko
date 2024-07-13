19a9f4d5970215df419ee1cdb26adfc8bf38309f
Bohdan Matsko <bm01271970@gmail.com>
2024-07-13 21:41:20 +0200

    goodJob

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -9,12 +9,12 @@ const Title = () => {
         Frontend Developer (junior) with great ambitions!
       </h2>
         <hr className={ css.line} />
-        <p className={css.mainParagraph}>
+        <span className={css.mainParagraph}>
           Able to understand technologies from complex distillation column to
           complex code! I aspire to develop in the field of IT, at the same time
           I am studying English, and I am also continuing my studies at the GoIT
           school
-        </p>
+        </span>
     </div>
   );
 }
